7e97f2f769183278abdff1d083dbd98b5582f803
Stuart Magowan <stuart.magowan@betfred.com>
2026-06-16 14:10:04 +0100

    fix: change public iam role to run.invoker

--- terraform/main.tf
+++ terraform/main.tf
@@ -326,7 +326,7 @@ resource "google_cloud_run_v2_service" "app" {
 resource "google_cloud_run_v2_service_iam_member" "public" {
   location = google_cloud_run_v2_service.app.location
   name     = google_cloud_run_v2_service.app.name
-  role     = "roles/run.viewer"
+  role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
